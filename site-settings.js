bdc4567715ab76184b16c93ccd3d3a67a35fd4eb
projektdtp <projekt.dtp@gmail.com>
2026-07-11 22:29:08 +0200

    Add files via upload

---
+++ site-settings.js
@@ -0,0 +1,8 @@
+﻿window.KS_SITE_SETTINGS = {
+  "phone": "578 414 690",
+  "whatsappNumber": "48578414690",
+  "serviceArea": "Województwo lubelskie · inne lokalizacje po uzgodnieniu",
+  "email": "",
+  "popupEnabled": true,
+  "popupDelayMs": 900
+};
